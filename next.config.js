b4c6602606c5157faaebcb9aeab3857b20536cd5
pattydubb <pattydub465@gmail.com>
2025-03-16 13:40:04 -0500

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add any custom Next.js configuration here
+  images: {
+    domains: ['images.clerk.dev'],
+  },
+}
+
+module.exports = nextConfig
